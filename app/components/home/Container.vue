fe2a02d9ab69fc314cc11a2a944e67050e5a1d6c
KUN1007 <yuyuyukunkunkun@gmail.com>
2025-12-16 15:47:48 +0800

    feat(prisma-schema): adding docs related prisma schema

--- app/components/home/Container.vue
+++ app/components/home/Container.vue
@@ -10,8 +10,6 @@ const { data } = await useFetch(`/api/home`, {
 <template>
   <div class="flex flex-col justify-between gap-3 rounded-lg lg:flex-row">
     <div class="w-full space-y-3">
-      <HomeCarousel />
-
       <HomeTopicContainer v-if="data" :topics="data.topics" />
 
       <HomeGalgameContainer v-if="data" :galgames="data.galgames" />
